23ece230d79a9301caa68071d598315839d90857
Deepti Raghavan <deeptir@mit.edu>
2017-07-25 10:56:03 -0400

    More prints for last rec packet

--- ctcp.hh
+++ ctcp.hh
@@ -150,7 +150,8 @@ template<class T>
 void CTCP<T>::send_data( double flow_size, bool byte_switched, int32_t flow_id, int32_t src_id ){
   cout << "In send data function about to send more data" << endl;
   tcp_handshake();
-  int last_sent_packet = 0;
+  int32_t last_sent_packet = 0;
+  int32_t last_rec_packet = 0;
 
   TCPHeader header, ack_header;
 
@@ -202,7 +203,7 @@ void CTCP<T>::send_data( double flow_size, bool byte_switched, int32_t flow_id, 
       
       memcpy( buf, &header, sizeof(TCPHeader) );
       std::cout << "sending packet " << header.seq_num << endl;
-      last_sent_packet = int(header.seq_num);
+      last_sent_packet =header.seq_num;
       socket.senddata( buf, packet_size, NULL );
       if ((cur_time - _last_send_time) / congctrl.get_intersend_time() > 10 ||
           seq_num >= _largest_ack + congctrl.get_the_window()) {
@@ -226,7 +227,7 @@ void CTCP<T>::send_data( double flow_size, bool byte_switched, int32_t flow_id, 
       timeout = min( timeout, _last_send_time + congctrl.get_intersend_time()*num_packets_per_link_rate_measurement - cur_time );
     
     sockaddr_in other_addr;
-    cout << "Waiting to receive data: last sent packet-> " << last_sent_packet << ", last rec packet =-> " << num_packets_transmitted <<endl;
+    cout << "Waiting to receive data: last sent packet-> " << last_sent_packet << ", last rec packet =-> " << last_rec_packet <<endl;
     if(socket.receivedata(buf, packet_size, timeout, other_addr) == 0) {
       cout << "Didn't get data yet " << endl;
       cur_time = current_timestamp(start_time_point);
@@ -242,6 +243,7 @@ void CTCP<T>::send_data( double flow_size, bool byte_switched, int32_t flow_id, 
     
     memcpy(&ack_header, buf, sizeof(TCPHeader));
     ack_header.seq_num++; // because the receiver doesn't do that for us yet
+    last_rec_packet = ack_header.seq_num;
     
     if (ack_header.src_id != src_id || ack_header.flow_id != flow_id){
       if(ack_header.src_id != src_id ){
